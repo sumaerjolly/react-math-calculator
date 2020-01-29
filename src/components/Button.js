dd438c43edafb595c6b3c4fee30e59af8e89df85
Sumaer Jolly <sumaerjolly@gmail.com>
2020-01-29 18:27:43 +0530

    Button Component added

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Button = (name) => {
-    
+const Button = (props) => {
+	<button type="button">{props.name}</button>
+};
+
+Button.propTypes = {
+	name: PropTypes.string,
 };
 
 export default Button;
