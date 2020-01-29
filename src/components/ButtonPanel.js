67bb11826b4f798c2f1a0d489b7bef167679d065
Sumaer Jolly <sumaerjolly@gmail.com>
2020-01-29 19:40:04 +0530

    stickler errors fix 3

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,33 +5,33 @@ const ButtonPanel = () => {
   return (
     <div className="buttonPanel">
       <div className="row-1">
-        <Button name="AC"/>
-        <Button name="+/-"/>
-        <Button name="%"/>
-        <Button name="+"/>
+        <Button name="AC" />
+        <Button name="+/-" />
+        <Button name="%" />
+        <Button name="+" />
       </div>
       <div className="row-2">
-        <Button name="7"/>
-        <Button name="8"/>
-        <Button name="9"/>
-        <Button name="X"/>
+        <Button name="7" />
+        <Button name="8" />
+        <Button name="9" />
+        <Button name="X" />
       </div>
       <div className="row-3">
-        <Button name="4"/>
-        <Button name="5"/>
-        <Button name="6"/>
-        <Button name="-"/>
+        <Button name="4" />
+        <Button name="5" />
+        <Button name="6" />
+        <Button name="-" />
       </div>
       <div className="row-4">
-        <Button name="1"/>
-        <Button name="2"/>
-        <Button name="3"/>
-        <Button name="+"/>
+        <Button name="1" />
+        <Button name="2" />
+        <Button name="3" />
+        <Button name="+" />
       </div>
       <div className="row-5">
-        <Button name="0"/>
-        <Button name="."/>
-        <Button name="="/>
+        <Button name="0" />
+        <Button name="." />
+        <Button name="=" />
       </div>
     </div>
   );
