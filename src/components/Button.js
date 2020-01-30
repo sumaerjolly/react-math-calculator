ec290a448f6fa18708b69e97a945ab7f2d041f5e
Sumaer Jolly <sumaerjolly@gmail.com>
2020-01-30 15:14:55 +0530

    added color and wide props to button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = ({ name, color, wide }) => (
-  <button className={} type="button">{ name }</button>
+  <button className={`${wide} ? 'wide' : 'not-wide'}`} style={{backgroundColor: color}} type="button">{ name }</button>
 );
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool.isRequired,
+};
+
+Button.defaultProps = {
+  color: '#f3913e',
 };
 
 export default Button;
