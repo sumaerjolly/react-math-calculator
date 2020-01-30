dbc761a7292a4e88854f7785ec62c107df162b2e
Sumaer Jolly <sumaerjolly@gmail.com>
2020-01-30 15:02:45 +0530

    added css for rows and basic button layout

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name }) => (
-  <button type="button">{ name }</button>
+const Button = ({ name, color, wide }) => (
+  <button className={} type="button">{ name }</button>
 );
 
 Button.propTypes = {
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,31 +3,31 @@ import Button from './Button';
 
 const ButtonPanel = () =>
 	<div className="buttonPanel">
-		<div className="row-1">
+		<div className="row">
 			<Button name="AC" />
 			<Button name="+/-" />
 			<Button name="%" />
 			<Button name="+" />
 		</div>
-		<div className="row-2">
+		<div className="row">
 			<Button name="7" />
 			<Button name="8" />
 			<Button name="9" />
 			<Button name="X" />
 		</div>
-		<div className="row-3">
+		<div className="row">
 			<Button name="4" />
 			<Button name="5" />
 			<Button name="6" />
 			<Button name="-" />
 		</div>
-		<div className="row-4">
+		<div className="row">
 			<Button name="1" />
 			<Button name="2" />
 			<Button name="3" />
 			<Button name="+" />
 		</div>
-		<div className="row-5">
+		<div className="row">
 			<Button name="0" />
 			<Button name="." />
 			<Button name="=" />
