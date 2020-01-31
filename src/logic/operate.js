21e5e4fd573a838a6d6aa7ae02db6e95ae0b7930
Sumaer Jolly <sumaerjolly@gmail.com>
2020-01-31 14:38:12 +0530

    added operate.js arithmetic operations

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,5 +1,24 @@
 import Big from 'big-js';
 
-const operate = (numberOne, numberTwo, operation) => {};
+const operate = (numberOne, numberTwo, operation) => {
+  const bigOne = Big(numberOne);
+  const bigTwo = Big(numberTwo);
+  if (operation === '+') {
+    return bigOne.plus(bigTwo);
+  }
+  if (operation === '-') {
+    return bigOne.minus(bigTwo);
+  }
+  if (operation === '÷') {
+    return bigOne.div(bigTwo);
+  }
+  if (operation === '×') {
+    return bigOne.times(bigTwo);
+  }
+  if (operation === '%') {
+    return bigOne.div(100);
+  }
+  return false;
+};
 
 export default operate;
